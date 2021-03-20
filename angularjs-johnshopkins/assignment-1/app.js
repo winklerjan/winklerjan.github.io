cc4d61276fb46f6020b39efb32cc3162555a03aa
Jan Winkler <janwinkler.mu@gmail.com>
2021-03-20 18:33:40 +0100

    folder restructuring

---
+++ angularjs-johnshopkins/assignment-1/app.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+    
+  angular.module('LunchChecker', [])
+  .controller('LunchCheckerController', ['$scope', DIController]);
+  
+  function DIController ($scope){
+
+    $scope.checkDishesCount = function () {
+        var dishesCount = countDishesInMenu($scope.menu);
+        
+        if (dishesCount == 0) 
+            $scope.message = 'Please enter some data first.';
+        else if (dishesCount > 0 && dishesCount <= 3)
+            $scope.message = 'Bon app!';
+        else $scope.message = 'Too much!';
+    }
+
+    function countDishesInMenu(string) {
+        return string != null ? string.split(',').length : 0;
+    }
+  }
+})();
